7a7e2056435bb8dea84a950cf8c37ed1d746522c
Angelo Cisneros <a.e.cisneros@gmail.com>
2015-05-17 01:48:14 -0700

    New distro

--- dist/js/ts-angular-bootstrap-calendar-tpls.js
+++ dist/js/ts-angular-bootstrap-calendar-tpls.js
@@ -57,7 +57,7 @@
             // CUSTOMIZATION: change eventEnd to be the same as eventStart
             function eventIsInPeriod(eventStart, eventEnd, periodStart, periodEnd) {
                 eventStart = moment(eventStart);
-                eventEnd = moment(eventStart);
+                eventEnd = moment(eventEnd);
                 periodStart = moment(periodStart);
                 periodEnd = moment(periodEnd);
                 return eventStart.isAfter(periodStart) && eventStart.isBefore(periodEnd) || eventEnd.isAfter(periodStart) && eventEnd.isBefore(periodEnd) || eventStart.isBefore(periodStart) && eventEnd.isAfter(periodEnd) || eventStart.isSame(periodStart) || eventEnd.isSame(periodEnd);
@@ -66,7 +66,7 @@
                 var startPeriod = moment(calendarDate).startOf(period);
                 var endPeriod = moment(calendarDate).endOf(period);
                 return allEvents.filter(function (event) {
-                    return eventIsInPeriod(event.startsAt, event.startsAt, startPeriod, endPeriod);
+                    return eventIsInPeriod(event.startsAt, event.endsAt, startPeriod, endPeriod);
                 });
             }
             function getBadgeTotal(events) {
@@ -91,7 +91,7 @@
                     var startPeriod = month.clone();
                     var endPeriod = startPeriod.clone().endOf('month');
                     var periodEvents = eventsInPeriod.filter(function (event) {
-                        return eventIsInPeriod(event.startsAt, event.startsAt, startPeriod, endPeriod);
+                        return eventIsInPeriod(event.startsAt, event.endsAt, startPeriod, endPeriod);
                     });
                     view.push({
                         label: startPeriod.format(calendarConfig.dateFormats.month),
@@ -117,7 +117,7 @@
                     var monthEvents = [];
                     if (inMonth) {
                         monthEvents = eventsInPeriod.filter(function (event) {
-                            return eventIsInPeriod(event.startsAt, event.startsAt, day, day.clone().endOf('day'));
+                            return eventIsInPeriod(event.startsAt, event.endsAt, day, day.clone().endOf('day'));
                         });
                     }
                     view.push({
@@ -160,10 +160,10 @@
                     dayCounter.add(1, 'day');
                 }
                 var eventsSorted = events.filter(function (event) {
-                    return eventIsInPeriod(event.startsAt, event.startsAt, startOfWeek, endOfWeek);
+                    return eventIsInPeriod(event.startsAt, event.endsAt, startOfWeek, endOfWeek);
                 }).map(function (event) {
                     var eventStart = moment(event.startsAt).startOf('day');
-                    var eventEnd = moment(event.startsAt).startOf('day');
+                    var eventEnd = moment(event.endsAt).startOf('day');
                     var weekViewStart = moment(startOfWeek).startOf('day');
                     var weekViewEnd = moment(endOfWeek).startOf('day');
                     var offset, span;
@@ -196,7 +196,7 @@
                 var dayHeightMultiplier = dayHeight / 60;
                 var buckets = [];
                 return eventsInPeriod.filter(function (event) {
-                    return eventIsInPeriod(event.startsAt, event.startsAt, moment(currentDay).startOf('day').toDate(), moment(currentDay).endOf('day').toDate());
+                    return eventIsInPeriod(event.startsAt, event.endsAt, moment(currentDay).startOf('day').toDate(), moment(currentDay).endOf('day').toDate());
                 }).map(function (event) {
                     if (moment(event.startsAt).isBefore(calendarStart)) {
                         event.top = 0;
@@ -224,7 +224,7 @@
                     buckets.forEach(function (bucket, bucketIndex) {
                         var canFitInThisBucket = true;
                         bucket.forEach(function (bucketItem) {
-                            if (eventIsInPeriod(event.startsAt, event.startsAt, bucketItem.partParameters.startTime, bucketItem.endsAt) || eventIsInPeriod(bucketItem.partParameters.startTime, bucketItem.endsAt, event.startsAt, event.startsAt)) {
+                            if (eventIsInPeriod(event.startsAt, event.endsAt, bucketItem.startsAt, bucketItem.endsAt) || eventIsInPeriod(bucketItem.startsAt, bucketItem.endsAt, event.startsAt, event.endsAt)) {
                                 canFitInThisBucket = false;
                             }
                         });
